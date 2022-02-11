dab9044f9e0d11ba2534463cb1dac77b4318ce9b
yuri.ranieri <yuri.ranieri@compasso.com.br>
2022-02-11 12:04:17 -0300

    aula 54 criando writer

--- ArquivoDelimitadoWriterJob/src/main/java/com/springbatch/arquivodelimitadowriterjob/writer/ArquivoDelimitadoWriter.java
+++ ArquivoDelimitadoWriterJob/src/main/java/com/springbatch/arquivodelimitadowriterjob/writer/ArquivoDelimitadoWriter.java
@@ -1,16 +1,28 @@
 package com.springbatch.arquivodelimitadowriterjob.writer;
 
 import com.springbatch.arquivodelimitadowriterjob.dominio.Cliente;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ArquivoDelimitadoWriter {
 
     @Bean
-    public ItemWriter<Cliente> leituraArquivoDelimitadoWriter() {
-        return items -> items.forEach(System.out::println);
+    @StepScope
+    public FlatFileItemWriter<Cliente> escritaArquivoDelimitadoWriter(
+            @Value("#{jobParameters['arquivoSaida']}") Resource arquivoSaida) {
+        return new FlatFileItemWriterBuilder<Cliente>()
+                .name("escritaArquivoDelimitadoWriter")
+                .resource(arquivoSaida)
+                .delimited() // tipo arquivo
+                .delimiter("||") // informa qual vai ser o delmitador (, ; \\ // || ...)
+                .names("nome", "sobrenome", "idade", "email")
+                .build();
     }
 
 }
