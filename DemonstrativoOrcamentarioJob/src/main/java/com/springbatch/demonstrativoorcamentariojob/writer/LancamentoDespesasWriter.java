d72a25f76e3a448792835aa97e8020a649411ffa
yuri.ranieri <yuri.ranieri@compasso.com.br>
2022-02-14 09:31:57 -0300

    aula 55 escrita em arq flat customizado

--- DemonstrativoOrcamentarioJob/src/main/java/com/springbatch/demonstrativoorcamentariojob/writer/LancamentoDespesasWriter.java
+++ DemonstrativoOrcamentarioJob/src/main/java/com/springbatch/demonstrativoorcamentariojob/writer/LancamentoDespesasWriter.java
@@ -1,8 +1,13 @@
 package com.springbatch.demonstrativoorcamentariojob.writer;
 
 import com.springbatch.demonstrativoorcamentariojob.dominio.GrupoLancamento;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.LineAggregator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.text.NumberFormat;
@@ -12,19 +17,31 @@ import java.text.SimpleDateFormat;
 public class LancamentoDespesasWriter {
 
     @Bean
-    public ItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter() {
-        return itens -> {
-            System.out.println("---- Demonstrativo orçamentário ----");
-            itens.forEach(grupo -> {
-                System.out.printf("[%d] %s - %s%n", grupo.getCodigoNaturezaDespesa(),
-                        grupo.getDescricaoNaturezaDespesa(),
-                        NumberFormat.getCurrencyInstance().format(grupo.getTotal()));
-                grupo.getLancamentos().forEach(lancamento ->
-                        System.out.printf("\t [%s] %s - %s%n",
-                                new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()),
-                                lancamento.getDescricao(),
-                                NumberFormat.getCurrencyInstance().format(lancamento.getValor())));
-            });
+    @StepScope
+    public FlatFileItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter(
+            @Value("#{jobParameters['arquivoSaida']}") Resource resource) {
+        return new FlatFileItemWriterBuilder<GrupoLancamento>()
+                .name("demonstrativoOrcamentarioWriter")
+                .resource(resource)
+                .lineAggregator(lineAggregator())
+                // informa o formato, que fica customizado, lineAggregator define a logica de agregacao da linha
+                .build();
+    }
+
+    private LineAggregator<GrupoLancamento> lineAggregator() {
+        return grupoLancamento -> {
+            var formatGrupo = String.format("[%d] %s - %s%n", grupoLancamento.getCodigoNaturezaDespesa(),
+                    grupoLancamento.getDescricaoNaturezaDespesa(),
+                    NumberFormat.getCurrencyInstance().format(grupoLancamento.getTotal()));
+
+            var stringBuilder = new StringBuilder();
+            grupoLancamento.getLancamentos().forEach(lancamento -> stringBuilder.append(
+                    String.format("\t [%s] %s - %s%n",
+                            new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()),
+                            lancamento.getDescricao(),
+                            NumberFormat.getCurrencyInstance().format(lancamento.getValor()))));
+            var formatLancamento = stringBuilder.toString();
+            return formatGrupo + formatLancamento;
         };
     }
 
